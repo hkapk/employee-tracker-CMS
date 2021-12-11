2c41f122afc428e7da739a3e3490147b5aad8393
Hank Kapka <hankkapka@gmail.com>
2021-12-11 11:54:25 -0600

    added function to initialize app with inquirer prompts

--- index.js
+++ index.js
@@ -2,5 +2,22 @@ const inquirer = require ('inquirer');
 const mysql = require ('mysql2');
 const cTable = require('console.table');
 
+function init() {
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "choices",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee role",
+            "Update an employee role"
+        ]
+    })
+};
 
+init();
 
