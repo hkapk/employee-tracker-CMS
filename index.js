baba866d6d2c1c6b72d46337ab9ae55ef427bdd7
Hank Kapka <hankkapka@gmail.com>
2021-12-11 17:32:44 -0600

    joined tables so showing employee table will show all required information

--- index.js
+++ index.js
@@ -76,7 +76,11 @@ function viewRoles() {
 
 //function to view all employees
 function viewEmployees () {
-    const sql = "SELECT * FROM employees;";
+    const sql = `SELECT
+    employees.id AS employee_id, employees.first_name, employees.last_name, roles.salary, departments.name AS department_name
+    FROM employees
+    JOIN roles ON employees.role_id = roles.id
+    JOIN departments ON roles.department_id = departments.id ;`;
     db.query(sql,
     function(err, res) {
         if (err) throw err
